0b362474fa9aea9a0d4e97ba542bbf636450ab6d
Nour-05 <nourayman3103@gmail.com>
2025-12-26 06:54:09 +0000

    auto commit

--- autoUpload.sh
+++ autoUpload.sh
@@ -3,4 +3,10 @@ git add .
 git commit -m "auto commit"
 git push origin main
 echo "Code uploaded successfully"
-curl http://192.168.1.25:8080/job/testJob/build?token=finalProjectToken
+
+if curl -s -o /dev/null -w "%{http_code}" \
+   "http://192.168.1.25:8080/job/testJob/build?token=finalProjectToken" | grep -q "201"; then
+  echo "Jenkins build triggered successfully."
+else
+  echo "Failed to trigger Jenkins build."
+fi
